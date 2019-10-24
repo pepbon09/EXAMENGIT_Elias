e0409441c03c6649396c23f267fc10db919d1887
Khoyac <55234923+Khoyac@users.noreply.github.com>
2019-10-24 19:13:46 +0200

    Primera prueba

---
+++ P411_TelaDeAraña.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class P411_TelaDeAraña {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y, suma, ele;
+		boolean cont = true;
+
+		while (true) {
+			x = sc.nextInt();
+			if ( x == 0) {
+				break;
+			}
+			
+			suma = 0;
+			ele = 0;
+			cont = true;
+			while (cont ) {
+				y = sc.nextInt();
+				if (y == 0) {
+					cont = false;
+				} else  {
+					suma = suma + y;
+					if (suma <= x ) {
+						ele++;
+					}		
+				}
+			}
+			
+			System.out.println(ele);
+			
+			
+		}
+		
+	}
+
+}
